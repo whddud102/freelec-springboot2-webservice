2ca853a5cccbf25a0771a6b41edec165be14024f
jongyoung <whddud102@naver.com>
2020-01-15 23:32:21 +0900

    2장 까지 실습 완료

---
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.jojoldu.book.springboot.service.posts;
+
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequestsDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestsDto requestsDto) {
+        return postsRepository.save(requestsDto.toEntitiy()).getId();
+    }
+}
