e8ba99c5cb1ecfccf07894b189769f8dc3874ad5
jongyoung <whddud102@naver.com>
2020-01-17 20:11:44 +0900

    2장 까지 실습 완료

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestsDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,8 +13,19 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestsDto requestDto) {
         return  postsService.save(requestDto);
     }
+
+    @PutMapping("api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestsDto) {
+        return postsService.update(id, requestsDto);
+    }
+
+    @GetMapping("api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
 }
--- src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot;
 
+import com.jojoldu.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
-
+@WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
         @Autowired
